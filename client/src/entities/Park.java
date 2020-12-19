e6aff471b42c6afabac3fe0980082fc8c4c08f16
avivs <avivs@LAPTOP-VDEVTM9A>
2020-12-19 15:52:27 +0200

    s

---
+++ client/src/entities/Park.java
@@ -0,0 +1,12 @@
+package entities;
+
+public class Park {
+
+		private String parkName;
+		private int maxVisitorsLimit;
+		private int stayTimeOfVisitors;
+		private int currentNumOfVisitors;
+		private int invitationVsVisitorGap;
+		
+		
+}
