e05d045c81e8ad165c53cd682489052a4d015acb
vladik120 <vladik120@gmail.com>
2020-12-25 20:52:42 +0200

    fix bug in new family and guide

--- client/controller/gui/DepartmentController.java
+++ client/controller/gui/DepartmentController.java
@@ -7,7 +7,6 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
